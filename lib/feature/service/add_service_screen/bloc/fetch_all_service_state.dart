5ebd1a184b067d4b53f64d3d4e47a57399e06226
Smonika28 <135808069+Smonika28@users.noreply.github.com>
2023-07-12 18:10:23 +0530

    12july
    worked on add service

---
+++ lib/feature/service/add_service_screen/bloc/fetch_all_service_state.dart
@@ -0,0 +1,134 @@
+// part of 'fetch_all_service_bloc.dart';
+//
+//
+//
+// abstract class FetchAllServiceState {}
+// class FetchAllServiceInitial extends FetchAllServiceState {}
+// class BranchsInitial extends FetchAllServiceState {}
+// class FetchAllServiceLoadingState extends FetchAllServiceState {}
+//
+// class FetchAllServiceLoadedState extends FetchAllServiceState {
+//   AllServiceModel allServiceModel;
+//   ServiceFieldModel? serviceFieldModel;
+//
+//
+//   FetchAllServiceLoadedState({required this.allServiceModel, this.serviceFieldModel});
+// }
+// class FetchAllServiceErrorState extends FetchAllServiceState {
+//   final String message;
+//   FetchAllServiceErrorState({required this.message});
+// }
+//
+// /// Set Service field
+//
+// abstract class FetchAllFieldState {}
+// class FetchAllFieldInitial extends FetchAllFieldState {}
+// class FetchAllServiceFieldLoading extends FetchAllFieldState{}
+// class FetchAllServiceDetailsFieldSetfaildState extends FetchAllFieldState{
+// }
+// class FetchAllServiceDetailsFieldSetDoneState extends FetchAllFieldState{
+//   ServiceFieldModel detailServiceModel;
+//   FetchAllServiceDetailsFieldSetDoneState({required this.detailServiceModel});
+//
+// }
+//
+
+
+//my code
+
+part of 'fetch_all_service_bloc.dart';
+
+
+
+abstract class FetchAllServiceState {}
+class FetchAllServiceInitial extends FetchAllServiceState {}
+class BranchsInitial extends FetchAllServiceState {}
+class FetchAllServiceLoadingState extends FetchAllServiceState {}
+
+class FetchAllServiceLoadedState extends FetchAllServiceState {
+  AllServiceModel allServiceModel;
+  ServiceFieldModel? serviceFieldModel;
+
+
+  FetchAllServiceLoadedState({required this.allServiceModel, this.serviceFieldModel});
+}
+class FetchAllServiceErrorState extends FetchAllServiceState {
+  final String message;
+  FetchAllServiceErrorState({required this.message});
+}
+
+/// Set Service field
+
+abstract class FetchAllFieldState {}
+class FetchAllFieldInitial extends FetchAllFieldState {}
+class FetchAllServiceFieldLoading extends FetchAllFieldState{}
+class FetchAllServiceDetailsFieldSetfaildState extends FetchAllFieldState{
+}
+class FetchAllServiceDetailsFieldSetDoneState extends FetchAllFieldState{
+  ServiceFieldModel detailServiceModel;
+  FetchAllServiceDetailsFieldSetDoneState({required this.detailServiceModel});
+
+}
+
+//add service
+abstract class AddAllServiceState {}
+class AddAllServiceInitial extends AddAllServiceState {}
+class AddAllServiceLoadingState extends AddAllServiceState {}
+class AddAllServiceLoadedState extends AddAllServiceState {
+  AllServiceModel allServiceModel;
+  ServiceFieldModel? serviceFieldModel;
+  AddAllServiceLoadedState({required this.allServiceModel, this.serviceFieldModel});
+}
+class AddAllServiceErrorState extends AddAllServiceState {
+  final String message;
+  AddAllServiceErrorState({required this.message});
+}
+
+/// Set Service field
+
+abstract class AddAllFieldState {}
+class AddAllFieldInitial extends AddAllFieldState {}
+class AddllServiceFieldLoading extends AddAllFieldState{}
+class AddAllServiceDetailsFieldSetfaildState extends AddAllFieldState{
+}
+class AddAllServiceDetailsFieldSetDoneState extends AddAllFieldState{
+  ServiceFieldModel detailServiceModel;
+  AddAllServiceDetailsFieldSetDoneState({required this.detailServiceModel});
+
+}
+
+
+
+//add service
+
+
+//add service
+// abstract class AddAllServiceState {}
+// class AddAllServiceInitial extends AddAllServiceState {}
+//  class BranchsInitial extends AddAllServiceState {}
+// class AddAllServiceLoadingState extends AddAllServiceState {}
+//
+// class AddAllServiceLoadedState extends AddAllServiceState {
+//   AllServiceModel allServiceModel;
+//   ServiceFieldModel? serviceFieldModel;
+//
+//
+//   AddAllServiceLoadedState({required this.allServiceModel, this.serviceFieldModel});
+// }
+// class AddAllServiceErrorState extends AddAllServiceState {
+//   final String message;
+//   AddAllServiceErrorState({required this.message});
+// }
+//
+// /// Set Service field
+//
+// abstract class AddAllFieldState {}
+// class AddAllFieldInitial extends AddAllFieldState {}
+// class AddllServiceFieldLoading extends AddAllFieldState{}
+// class AddAllServiceDetailsFieldSetfaildState extends AddAllFieldState{
+// }
+// class AddAllServiceDetailsFieldSetDoneState extends AddAllFieldState{
+//   ServiceFieldModel detailServiceModel;
+//   AddAllServiceDetailsFieldSetDoneState({required this.detailServiceModel});
+//
+// }
